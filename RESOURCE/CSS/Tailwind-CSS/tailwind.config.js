e75756b549873745b87df8fb4e15546931ddd3e1
Aditya <118204758+wpsadi@users.noreply.github.com>
2023-07-12 18:57:09 +0000

    del

--- RESOURCE/CSS/Tailwind-CSS/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["/workspaces/CODE-SPACE/RESOURCE/CSS/Tailwind-CSS/dist/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
