4301e3fc0be5d92bf0c3e266179e14b8d3d5024e
Aditya <118204758+wpsadi@users.noreply.github.com>
2023-07-16 18:44:42 +0000

    config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/workspaces/CODE-SPACE/RESOURCE/CSS/Tailwind-CSS/dist", "/workspaces/CODE-SPACE/RESOURCE/CSS/Tailwind-CSS/ASSIGNMENT"],
+  content: ["/workspaces/CODE-SPACE/RESOURCE/CSS/Tailwind-CSS/Tailwind-CSS-(basic)/dist", "/workspaces/CODE-SPACE/RESOURCE/CSS/Tailwind-CSS/Tailwind-CSS-(basic)/ASSIGNMENT"],
   theme: {
     extend: {},
   },
