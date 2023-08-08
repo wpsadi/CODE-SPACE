f395422898e741b120c25d7b7888736abd60b997
wpsadi <adityaislegend@outlook.com>
2023-08-08 12:52:39 +0530

    JS

--- RESOURCE/JavaScript/Part-02-JavaScript-Fundamentals/01 Intro to functions/index.js
+++ RESOURCE/JavaScript/Part-02-JavaScript-Fundamentals/01 Intro to functions/index.js
@@ -1,12 +1,46 @@
-function fibo(n,a=0,b=1){
-   n-=1
-   c=a
-   a=a+b
-   b=c
-   if (n>0){
-      fibo(n,a,b)
-   }
+// let a = 10;
+// let b = 20;
 
+// function add(a=10,b=20){
+//    console.log(a+b)
+// }
+
+// add() //function calling / function invokation
+// add(5,4)
+// add(20,5)
+// arguments
+
+// let sum = function add(a=10, b=20){
+//    console.log(a+b)
+// }
+
+// console.log(sum)
+
+// // first class citizen
+
+// function x(callback){
+//    console.log("hello ")
+//    callback()
+// }
+
+// function y(){
+//    console.log("bye")
+// }
+
+// x(y)
+
+
+
+// function add(a,b) {
+//    return function sum(){
+
+//    }
+// }
+
+function add(a,b){
+   return(a+b)
 }
 
-console.log(fibo(20))
+
+let  s= add(10,20)
+console.log(s)
