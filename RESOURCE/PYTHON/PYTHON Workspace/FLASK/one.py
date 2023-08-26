32b79c060d819668cd9da58710daac75eca50b93
wpsadi <adityaislegend@outlook.com>
2023-08-27 02:10:59 +0530

    py

--- RESOURCE/PYTHON/PYTHON Workspace/FLASK/one.py
+++ RESOURCE/PYTHON/PYTHON Workspace/FLASK/one.py
@@ -1,13 +1,3 @@
-from flask import *
-app=Flask(__name__)
-
-@app.route("/")
-def hello():
-    return render_template('index.html')
-
-@app.route("/login")
-def login():
-    # topic=input().strip()
-    topic="hello"
-    return render_template('login.html',topic=topic)
-app.run(debug=True)
+while True:
+    x=input("=>")
+    print(len(x), len(x)%8)
