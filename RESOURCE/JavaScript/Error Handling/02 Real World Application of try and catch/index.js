db30631f44fb6cd878f6bac511374cef5c97479c
wpsadi <adityaislegend@outlook.com>
2023-09-24 19:58:35 +0530

    js

---
+++ RESOURCE/JavaScript/Error Handling/02 Real World Application of try and catch/index.js
@@ -0,0 +1,13 @@
+try{
+   console.log("hello")
+   console.log(a)
+   console.log("try-end")
+}
+catch{
+   console.log("err handled")
+}
+finally{
+   console.assertlog("finally")
+}
+
+console.log("handled")
