f4e33e566b0e1a89f819aed922a26d93cebdb334
wpsadi <adityaislegend@outlook.com>
2023-09-28 17:18:13 +0530

    save

---
+++ RESOURCE/C language/Variables/Hello.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+   int number = 22;
+   float pi = 3.14;
+   char hash = 'y' ;
+   // hi there
+   /*i love c and this is a
+   mutiline comment*/
+
+   printf("value of PI is %f \nsee this line is in new line\n\n",pi);
+   // %d : integer
+   // %f : float
+   // %c : char
+
+   char status;
+   printf("status is :");
+   // scanf("%c", &status);
+   return 1;
+}
+
