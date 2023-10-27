db414e687c058eb487d8df4a33f159884aa228d5
wpsadi <adityaislegend@outlook.com>
2023-10-27 20:22:10 +0530

    save

--- RESOURCE/JavaScript/JavaScript Workspace/JavaScript-Fundamentals/Part-01-JavaScript-Fundamentals/01 Fundamentals (Variables, Value and Data types, Ternary Operators, Switch case, Scope)/module2.js
+++ RESOURCE/JavaScript/JavaScript Workspace/JavaScript-Fundamentals/Part-01-JavaScript-Fundamentals/01 Fundamentals (Variables, Value and Data types, Ternary Operators, Switch case, Scope)/module2.js
@@ -1,5 +1,5 @@
 // import {module_Function, module_variable} from "./module1.js"
 
-var {module_Function} = require("./module1.js")
+let {module_Function} = require("./module1.js")
 
 module_Function()
