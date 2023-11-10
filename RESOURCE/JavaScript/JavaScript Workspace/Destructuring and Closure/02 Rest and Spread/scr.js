3478e57d62ab5a1bf3e97a0783cf34b2c392b58b
wpsadi <adityaislegend@outlook.com>
2023-11-10 15:34:40 +0530

    save

---
+++ RESOURCE/JavaScript/JavaScript Workspace/Destructuring and Closure/02 Rest and Spread/scr.js
@@ -0,0 +1,12 @@
+// spread and rest operator
+
+// spread
+   // -- can create new arrays
+   let arr = [1,2,3,5]
+   let newArr = Array
+
+
+   // -- can add new values
+   // -- concatinate 2 arrays
+   // -- spread an array of arguments as individual
+   // -- can be use with strings, objects
