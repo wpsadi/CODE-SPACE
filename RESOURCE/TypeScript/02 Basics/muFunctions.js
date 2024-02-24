8bc4832de4d6a27cabf5b0d1d611360acba5206c
wpsadi <adityaislegend@outlook.com>
2024-02-24 23:49:56 +0530

    functions

---
+++ RESOURCE/TypeScript/02 Basics/muFunctions.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addNums(num1, num2) {
+    return num1 + num2;
+}
+var x = addNums(3, 3);
+var loginUser = function (username, password, paid) {
+    if (paid === void 0) { paid = false; }
+    return "hi";
+};
+
+var user = loginUser("aditya", "adityaislegend@outlook.com");
--- RESOURCE/TypeScript/02 Basics/muFunctions.ts
+++ RESOURCE/TypeScript/02 Basics/muFunctions.ts
@@ -1,5 +1,13 @@
-function addNums(num:number){
-    return num+2
+function addNums(num1:number,num2:number):number{
+    return num1+num2
 }
 
-let x = addNums(3)
+let x = addNums(3,3)
+
+let loginUser = (username:string,password:string, paid:boolean = false):string=>{
+    return "hi"
+}
+
+let user = loginUser("aditya","adityaislegend@outlook.com")
+
+export {}
